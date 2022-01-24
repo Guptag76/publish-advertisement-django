243903c7c61e9c70c3de1eff8aa38cc225d23262
Guptag76 <kkr9771806644@gmail.com>
2022-01-24 23:40:38 +0530

    Update views.py

--- website/views.py
+++ website/views.py
@@ -97,7 +97,10 @@ def open_data_from_rest():
     result=Hero.objects.all()
     val=HeroSerializer(result,many=True)
     data=val.data
-    data=eval(data[0]['my_data'])
+    try:
+        data=eval(data[0]['my_data'])
+    except:
+        data={}
     return data
     
 
